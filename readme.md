592a311a8b5dc467d56f1b212aed06a036a8869d
yakisova41 <yakisoba5emon@gmail.com>
2022-02-21 20:59:57 +0900

    v1.0

---
+++ readme.md
@@ -0,0 +1,44 @@
+# Yakisova41/routing
+## Usage
+```php
+require_once 'vendor/autoload.php';
+
+use Yakisova41\Routing\Routing;
+
+//The first argument is the request path and the second argument is the request method
+$router = new Routing(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH),$_SERVER['REQUEST_METHOD']);
+
+/*    Defining Routing
+--------------------------
+first parameter
+|Please enter a path
+|{enclose in brackets} parameter can be set
+
+second parameter
+|Enter the method as an array in all capital letters
+
+third parameter
+|Write the process
+*/
+
+$router->route('/',['GET'],function(){
+    echo 'Toppage!!!';
+});
+
+$router->route('/page/{id}',['GET'],function($param){
+    echo 'page';
+    echo $param['id'];
+});
+
+$router->route('/all',['GET','POST'],function(){
+    echo 'Both post and get are welcome!';
+});
+
+/*    Start routing
+--------------------------
+ The argument specifies the behavior at 404, and can be omitted (in that case, the default 404 page will be displayed)
+*/
+$router->run(function(){
+    echo '404 not found';
+});
+
---
+++ src/Routing.php
@@ -0,0 +1,71 @@
+<?php
+namespace Yakisova41\Routing;
+
+class Routing{
+    public function __construct($requestpath = false,$requestmethod = false){
+        if(!$requestpath||!$requestmethod){
+            trigger_error("Yakisova41/Routing Specify the request path and request method as arguments. example:\"new Routing(path,method)\"",E_USER_ERROR);      
+        }
+        else{
+            $this->notfound = false;
+            $this->reqpath = $requestpath;
+            $this->method = $requestmethod;
+            $this->routings = ['GET'=>[],'POST'=>[]];
+        }
+    }
+
+    public function route($path,$method,$function,$ifnotfound = false){
+        foreach($method as $key => $val){
+            $this->routings[$val][] = ["$path",$function];
+        }
+    }   
+
+    public function run($notfound = false){
+        foreach($this->routings[$this->method] as $key => $router){
+            if($router[0] == $this->reqpath){
+                $router[1](false);
+            }
+            elseif($this->parameter($router[0],$this->reqpath)[0]){
+                $router[1]($this->parameter($router[0],$this->reqpath)[1]);
+            }
+            else{
+                if(!$notfound){
+                    print('<h1>Not Found</h1><p>The requested URL was not found on this server</p><hr><p>Yakisova41/routing</p>');
+                }else{
+                    $notfound();
+                }
+
+            }
+        }   
+    }
+
+    public function parameter($path,$req){
+        $plodepath = explode('/',$path);
+        $plodereqpath = explode('/',$req);
+        $params = [];
+
+        foreach($plodepath as $key => $val){
+            if(preg_match("/^{.*}$/",$val)){
+                if(isset($plodereqpath[$key])){
+                    $paramname = ltrim($val,'{');
+                    $paramname = rtrim($paramname,'}');
+
+                    $params[$paramname] =  $plodereqpath[$key];
+                    $plodepath[$key] = $plodereqpath[$key];
+                }
+            }
+        }
+        
+        $makepath = '';
+        foreach($plodepath as $key => $val){
+            $makepath = $makepath.$val.'/';
+        }
+        $makepath = substr($makepath,0,-1);
+
+        if($req == $makepath){
+            return [true,$params];
+        }else{
+            return [false];
+        }
+    }
+}
